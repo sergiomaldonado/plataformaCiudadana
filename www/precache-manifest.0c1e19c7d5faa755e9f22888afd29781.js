4071f523da28bde7e113aa4fdfba843b61408ebf
Sergio Maldonado <srgiomaldonado@gmail.com>
2019-04-28 17:20:45 -0500

    nueva app

---
+++ www/precache-manifest.0c1e19c7d5faa755e9f22888afd29781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4594d9ba91b8f4ffae5ccda4acd0346",
+    "url": "../www/index.html"
+  },
+  {
+    "revision": "211dfaa8f11838561d62",
+    "url": "../www/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "../www/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "211dfaa8f11838561d62",
+    "url": "../www/static/js/main.7ada62be.chunk.js"
+  },
+  {
+    "revision": "f709b49592814e017119",
+    "url": "../www/static/js/runtime~main.4d52c8cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "../www/static/media/logo.5d5d9eef.svg"
+  }
+]);
